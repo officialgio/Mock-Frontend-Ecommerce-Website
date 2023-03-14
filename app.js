625e13302572f8db1d5d0b0030dbb641f589b7fd
officialgio121 <officialgio121@gmail.com>
2023-03-13 21:47:31 -0500

    Added transitions

---
+++ app.js
@@ -0,0 +1,78 @@
+// Tl's for transitions
+const tlLeave = gsap.timeline({
+    defaults: {
+        duration: 0.75,
+        ease: 'Power2.easeOut'
+    }
+});
+
+const tlEnter = gsap.timeline({
+    defaults: {
+        duration: 0.75,
+        ease: 'Power2.easeOut'
+    }
+});
+
+// Functions for leave and enter animations
+/**
+ * Once leave the animation have the current arrow fade out and go downwards vertically
+ * Have the product image do the same thing
+ * Then, have the text fade out and go downwards vertically
+ * @param {current} current 
+ * @param {done} done 
+ * @returns 
+ */
+const leaveAnimation = (current, done) => {
+    const product = current.querySelector('.image__container');
+    const text = current.querySelector('.showcase__text');
+    const circles = current.querySelectorAll('.circle');
+    const arrow = current.querySelector('.showcase-arrow');
+    return (
+        tlLeave.fromTo(arrow, { opacity: 1, y: 0 }, { opacity: 0, y: 50 }),
+        tlLeave.fromTo(product, { y: 0, opacity: 1 }, { y: 100, opacity: 0 }, '<'),
+        tlLeave.fromTo(text, { y: 0, opacity: 1 }, { opacity: 0, y: 100, onComplete: done }, '<'),
+        tlLeave.fromTo(circles, { y: 0, opacity: 1 }, { y: -200, opacity: 0, stagger: 0.15, ease: 'back.out(1.7)', duration: 1 }, '<')
+    )
+}
+
+// Function for enter animations
+/**
+ * Enter animation is the reverse of leave animation
+ * @param {current} current 
+ * @param {done} done 
+ * @returns 
+ */
+const enterAnimation = (current, done) => {
+    const product = current.querySelector('.image__container');
+    const text = current.querySelector('.showcase__text');
+    const circles = current.querySelectorAll('.circle');
+    const arrow = current.querySelector('.showcase-arrow');
+    return (
+        tlLeave.fromTo(arrow, { opacity: 0, y: 50 }, { opacity: 1, y: 0 }),
+        tlLeave.fromTo(product, { y: -100, opacity: 0 }, { y: 0, opacity: 1 }, '<'),
+        tlLeave.fromTo(text, { y: 100, opacity: 0 }, { opacity: 1, y: 0, onComplete: done }, '<'),
+        tlLeave.fromTo(circles, { y: -200, opacity: 0 }, { y: 0, opacity: 1, stagger: 0.15, ease: 'back.out(1.7)', duration: 1 }, '<')
+    
+    
+    )
+}
+
+// Transition
+barba.init({
+    preventRunning: true, // prevent clicking multiple times bug fix
+    transitions: [
+        {
+            leave(data) {
+                const done = this.async();
+                let current = data.current.container;
+                leaveAnimation(current, done);
+            }, 
+            enter(data) {
+                const done = this.async();
+                let next = data.next.container;
+                enterAnimation(next, done);
+                
+            },
+        },
+    ],
+});
